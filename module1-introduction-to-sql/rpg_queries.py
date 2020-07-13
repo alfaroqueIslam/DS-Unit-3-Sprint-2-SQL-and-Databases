1e95f029dd701b3bcc6eaeffca36a40419475800
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-13 17:20:27 -0400

    Create rpg_queries.py

---
+++ module1-introduction-to-sql/rpg_queries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Our Questions:
+    How many total Characters are there?
+    How many of each specific subclass?
+    How many total Items?
+    How many of the Items are weapons? How many are not?
+    How many Items does each character have? (Return first 20 rows)
+    How many Weapons does each character have? (Return first 20 rows)
+    On average, how many Items does each Character have?
+    On average, how many Weapons does each character have?
+"""
+import sqlite3
+conn = sqlite3.connect('rpg_db.sqlite3')
+curs = conn.cursor()
+# How many total Characters? Table: charactercreator_character
+count_characters = 'SELECT COUNT(*) FROM charactercreator_character;'
+print(curs.execute(count_characters).fetchall())
