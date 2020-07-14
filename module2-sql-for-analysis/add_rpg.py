2d7d1f77e5b32a0673e5e53d6fc5306eb9144b59
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-14 17:30:32 -0400

    commit

--- module2-sql-for-analysis/add_rpg.py
+++ module2-sql-for-analysis/add_rpg.py
@@ -18,3 +18,51 @@ sl_curs = sl_conn.cursor()
 get_characters = 'SELECT COUNT(*) FROM charactercreator_character;'
 characters = sl_curs.execute(get_characters).fetchall()
 
+print(characters)
+
+create_character_table = """
+CREATE TABLE charactercreator_character (
+  character_id SERIAL PRIMARY KEY,
+  name VARCHAR(30),
+  level INT,
+  exp INT,
+  hp INT,
+  strength INT,
+  intelligence INT,
+  dexterity INT,
+  wisdom INT
+);
+"""
+
+pg_curs = pg_conn.cursor()
+#pg_curs.execute(create_character_table)
+#pg_conn.commit()
+
+show_tables = """
+SELECT
+   *
+FROM
+   pg_catalog.pg_tables
+WHERE
+   schemaname != 'pg_catalog'
+AND schemaname != 'information_schema';
+"""
+pg_curs.execute(show_tables)
+pg_curs.fetchall()
+
+for character in characters:
+  insert_character = """
+    INSERT INTO charactercreator_character
+    (name, level, exp, hp, strength, intelligence, dexterity, wisdom)
+    VALUES """ + str(character[1:]) + ";"
+  pg_curs.execute(insert_character)
+
+pg_curs.execute('SELECT * FROM charactercreator_character LIMIT 5;')
+pg_curs.fetchall()
+
+pg_conn.commit()
+
+pg_curs.execute('SELECT * FROM charactercreator_character;')
+pg_characters = pg_curs.fetchall()
+
+print(len(pg_characters))
