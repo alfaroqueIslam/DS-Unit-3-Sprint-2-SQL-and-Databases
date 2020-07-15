64bb630b96f0cfa0517b3b636d17fe3d7a61d87b
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-15 16:52:14 -0400

    commit

--- module3-nosql-and-document-oriented-databases/add_rpg_mongo.py
+++ module3-nosql-and-document-oriented-databases/add_rpg_mongo.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
+
+import pymongo
+import sqlite3
+
+
+sl_conn = sqlite3.connect('rpg_db.sqlite3')
+sl_curs = sl_conn.cursor()
+
+get_characters = 'SELECT * FROM charactercreator_character;'
+characters = sl_curs.execute(get_characters).fetchall()
 
 client = pymongo.MongoClient("mongodb+srv://alfaroque5:<password>@cluster0.jij4e.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.test
+
+character_docs = []
+for character in characters:
+  doc = {'character_id' : character[0],
+  'name' : character[1],
+  'level' : character[2],
+  'exp' : character[3],
+  'hp' : character[4],
+  'strength' : character[5],
+  'intelligence' : character[6],
+  'dexterity' : character[7],
+  'wisdom' : character[8]}
+  character_docs.append(doc)
+
+db.test.insert_many(character_docs)
+
+
+
+
