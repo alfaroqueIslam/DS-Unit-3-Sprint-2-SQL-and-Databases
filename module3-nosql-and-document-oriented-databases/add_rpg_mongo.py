afb63736bb079bea9fa772432cca508508139142
Alfaroque Islam <alfaroque5@gmail.com>
2020-07-16 23:06:59 -0400

    commit

--- module3-nosql-and-document-oriented-databases/add_rpg_mongo.py
+++ module3-nosql-and-document-oriented-databases/add_rpg_mongo.py
@@ -10,6 +10,15 @@ sl_curs = sl_conn.cursor()
 get_characters = 'SELECT * FROM charactercreator_character;'
 characters = sl_curs.execute(get_characters).fetchall()
 
+get_inventory = 'SELECT * FROM charactercreator_character_inventory;'
+inventory = sl_curs.execute(get_inventory).fetchall()
+
+get_items = 'SELECT * FROM armory_item;'
+items = sl_curs.execute(get_items).fetchall()
+
+get_weapons = 'SELECT * FROM armory_weapon;'
+weapons = sl_curs.execute(get_weapons).fetchall()
+
 client = pymongo.MongoClient("mongodb+srv://alfaroque5:dAsF9ZRMS1gM2Wah@cluster0.jij4e.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.test
 
@@ -25,6 +34,26 @@ for character in characters:
   'dexterity' : character[7],
   'wisdom' : character[8]}
   character_docs.append(doc)
+
+inventory_docs = []
+for invent in inventory:
+  doc = {'id' : inventory[0], 
+  'character_id' : inventory[1],
+  'item_id' : inventory[2]}
+  
+  inventory_docs.append(doc)
+
+item_docs = []
+for item in items:
+  doc = {'item_id' : item[0]}
+
+  item_docs.append(doc)
+
+weapon_docs = []
+for weapon in weapons:
+  doc = {'item_ptr_id' : weapon[0]}
+
+  weapon_docs.append(doc)
 
 db.test.insert_many(character_docs)
 
